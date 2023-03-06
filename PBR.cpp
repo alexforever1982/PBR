f0e92b005c6a6d52c828fb678042eaf77e173607
Alexander Yakimov <63421555+alexforever1982@users.noreply.github.com>
2023-03-06 19:19:46 +0300

    Add files via upload
    minimal program and shader

---
+++ PBR.cpp
@@ -0,0 +1,80 @@
+
+#include <iostream>
+
+#include "GLAD/glad.h"
+#include "Shader.h"
+
+#include "GLFW/glfw3.h"
+
+#include <glm/glm.hpp>
+
+//==============================================================================
+
+void OnResize(GLFWwindow *window, int width, int height);
+
+void ProcessInput(GLFWwindow *window);
+
+void Render();
+
+//==============================================================================
+
+int main()
+{
+	const unsigned int SCR_WIDTH  = 1280;
+	const unsigned int SCR_HEIGHT = 720;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	auto *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "PBR", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, OnResize);
+
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		ProcessInput(window);
+		Render();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	std::cout << "OK" << std::endl;
+	std::cin.get();
+	return 0;
+}
+
+//==============================================================================
+
+void ProcessInput(GLFWwindow *window)
+{
+	const auto esc = glfwGetKey(window, GLFW_KEY_ESCAPE);
+	if (esc == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+//==============================================================================
+
+void OnResize(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+//==============================================================================
+
+void Render()
+{
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+//==============================================================================
