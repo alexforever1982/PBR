9beb5025f2ab834b2246e00a558411c36140853e
Alexander Yakimov <63421555+alexforever1982@users.noreply.github.com>
2023-03-07 15:57:03 +0300

    hello triangle

--- PBR.cpp
+++ PBR.cpp
@@ -15,7 +15,16 @@ void OnResize(GLFWwindow *window, int width, int height);
 
 void ProcessInput(GLFWwindow *window);
 
+void Prepare();
+
 void Render();
+
+//==============================================================================
+
+unsigned int VAO;
+unsigned int VBO;
+
+Shader shader;
 
 //==============================================================================
 
@@ -36,6 +45,8 @@ int main()
 
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
+	Prepare();
+
 	while (!glfwWindowShouldClose(window))
 	{
 		ProcessInput(window);
@@ -44,6 +55,9 @@ int main()
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
 	return 0;
@@ -69,10 +83,46 @@ void OnResize(GLFWwindow *window, int width, int height)
 
 //==============================================================================
 
+void Prepare()
+{
+	shader.Load("simple.vs", "simple.fs");
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	float data[] =
+	{
+		 0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+//==============================================================================
+
 void Render()
 {
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	shader.Use();
+
+	glBindVertexArray(VAO);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glBindVertexArray(0);
 }
 
 //==============================================================================
