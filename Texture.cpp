2cc867f1c536fe121d317fe2c40e08fd45b413c1
Alexander Yakimov <63421555+alexforever1982@users.noreply.github.com>
2023-03-07 10:12:57 +0300

    hdr texture

--- Texture.cpp
+++ Texture.cpp
@@ -37,6 +37,24 @@ void Texture::Init(const unsigned char *data) noexcept
 
 //==============================================================================
 
+void Texture::Init(const float *data) noexcept
+{
+	if (!texture)
+	{
+		glGenTextures(1, &texture);
+	}
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, width, height, 0, GL_RGB, GL_FLOAT, data);
+
+	SetParametersHDR();
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+//==============================================================================
+
 void Texture::SetParameters() noexcept
 {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -44,6 +62,17 @@ void Texture::SetParameters() noexcept
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+}
+
+//==============================================================================
+
+void Texture::SetParametersHDR() noexcept
+{
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
 //==============================================================================
@@ -69,6 +98,22 @@ void Texture::Load(const std::string &path, bool flip) noexcept
 {
 	stbi_set_flip_vertically_on_load(flip);
 	const auto data = stbi_load(path.c_str(), &width, &height, &components, 0);
+	if (data)
+	{
+		Init(data);
+		stbi_image_free(data);
+		return;
+	}
+
+	std::cout << "texture " << path << " not found" << std::endl;
+}
+
+//==============================================================================
+
+void Texture::LoadHDR(const std::string &path, bool flip) noexcept
+{
+	stbi_set_flip_vertically_on_load(flip);
+	const auto data = stbi_loadf(path.c_str(), &width, &height, &components, 0);
 	if (data)
 	{
 		Init(data);
