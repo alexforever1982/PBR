d4b4ffac21f86121a5bb1c8a03cb4c352e3ba872
Alexander Yakimov <63421555+alexforever1982@users.noreply.github.com>
2023-03-11 09:35:22 +0300

    Update Cubemap.h

--- Cubemap.h
+++ Cubemap.h
@@ -17,9 +17,10 @@ private:
 	void SetParameters() noexcept;
 
 public:
-	Cubemap() noexcept;
+	Cubemap()  noexcept;
+	~Cubemap() noexcept;
 
-	void Load(const std::vector<std::string> &faces) noexcept;
+	void Load(const std::vector<std::string> &faces, bool flip) noexcept;
 
 	void Bind(unsigned int texture_unit = 0) const noexcept;
 	static void Unbind() noexcept;
