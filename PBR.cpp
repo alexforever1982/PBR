d412a65596edd59c201ae79fee1b7ac2fee6d43f
Alexander Yakimov <63421555+alexforever1982@users.noreply.github.com>
2023-03-11 11:09:16 +0300

    Update PBR.cpp

--- PBR.cpp
+++ PBR.cpp
@@ -46,9 +46,6 @@ Skybox  *skybox;
 
 Sphere *sphere;
 
-unsigned int VAO;
-unsigned int VBO;
-
 //==============================================================================
 
 int main()
@@ -83,10 +80,7 @@ int main()
 	}
 
 	Cleanup();
-
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-
+	
 	glfwTerminate();
 	return 0;
 }
@@ -208,27 +202,6 @@ void Prepare()
 	cubemap->Load(cubemap_faces, false);
 
 	sphere = new Sphere;
-
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	float data[] =
-	{
-		 0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
-	};
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_BUFFER, 0);
-	glBindVertexArray(0);
 }
 
 //==============================================================================
