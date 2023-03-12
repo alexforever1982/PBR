6b9c9af9c8d04bab9bc5e80f2ca98313a1002e52
Alexander Yakimov <63421555+alexforever1982@users.noreply.github.com>
2023-03-12 11:31:20 +0300

    Update phong.fs

--- shaders/phong.fs
+++ shaders/phong.fs
@@ -1,10 +1,15 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+
 struct Material
 {
 	sampler2D diffuse;
 	sampler2D specular;
+	sampler2D normal;
 	float shininess;
 };
 
@@ -16,13 +21,11 @@ struct Light
 	vec3 specular;
 };
 
-in vec3 Normal;
-in vec3 FragPos;
-in vec2 TexCoords;
-
-uniform vec3 viewPos;
 uniform Material material;
 uniform Light light;
+uniform vec3 camera;
+
+vec3 getNormalFromMap();
 
 void main()
 {
@@ -31,12 +34,13 @@ void main()
 	
 	// diffuse
 	vec3 norm = normalize(Normal);
+	norm = getNormalFromMap();
 	vec3 lightDir = normalize(light.position - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
 	
 	// specular
-	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 viewDir = normalize(camera - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	vec3 halfwayDir = normalize(lightDir + viewDir);
 	float spec = pow(max(dot(viewDir, halfwayDir), 0.0), material.shininess);
@@ -44,4 +48,21 @@ void main()
 	
 	vec3 result = ambient + diffuse + specular;
 	FragColor = vec4(result, 1.0);
+}
+
+vec3 getNormalFromMap()
+{
+    vec3 tangentNormal = texture(material.normal, TexCoords).xyz * 2.0 - 1.0;
+
+    vec3 Q1  = dFdx(FragPos);
+    vec3 Q2  = dFdy(FragPos);
+    vec2 st1 = dFdx(TexCoords);
+    vec2 st2 = dFdy(TexCoords);
+
+    vec3 N   = normalize(Normal);
+    vec3 T  = normalize(Q1*st2.t - Q2*st1.t);
+    vec3 B  = -normalize(cross(N, T));
+    mat3 TBN = mat3(T, B, N);
+
+    return normalize(TBN * tangentNormal);
 }
