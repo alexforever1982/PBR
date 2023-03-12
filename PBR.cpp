18deae7ebfdb64539fd19cdb4bc9276cbe205718
Alexander Yakimov <63421555+alexforever1982@users.noreply.github.com>
2023-03-12 11:48:34 +0300

    Update PBR.cpp
    working pbr

--- PBR.cpp
+++ PBR.cpp
@@ -35,18 +35,12 @@ const auto height = 720u;
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
-////Shader simple_shader;
-////Shader phong_shader;
-////Shader skybox_shader;
 Shader pbr_shader;
 Shader rect2cubemap_shader;
 Shader irradiance_shader;
 Shader prefilter_shader;
 Shader brdf_shader;
 Shader background_shader;
-
-////Texture *diffuse_map;
-////Texture *specular_map;
 
 Texture *albedo_map;
 Texture *normal_map;
@@ -59,8 +53,6 @@ Cubemap *env_cubemap;
 Cubemap *irradiance_map;
 Cubemap *prefilter_map;
 Texture *brdfLUT_texture;
-
-////Cubemap *cubemap;
 
 Skybox  *skybox;
 Sphere *sphere;
@@ -190,19 +182,13 @@ void OnScroll(GLFWwindow *window, double dx, double dy)
 
 void Prepare()
 {
-	////simple_shader       .Load("shaders\\simple.vs",     "shaders\\simple.fs");
-	////phong_shader        .Load("shaders\\phong.vs",      "shaders\\phong.fs");
-	////skybox_shader       .Load("shaders\\skybox.vs",     "shaders\\skybox.fs");
 	pbr_shader          .Load("shaders\\pbr.vs",        "shaders\\pbr.fs");
 	rect2cubemap_shader .Load("shaders\\cubemap.vs",    "shaders\\rect2cubemap.fs");
 	irradiance_shader   .Load("shaders\\cubemap.vs",    "shaders\\irradiance.fs");
 	prefilter_shader    .Load("shaders\\cubemap.vs",    "shaders\\prefilter.fs");
 	brdf_shader         .Load("shaders\\brdf.vs",       "shaders\\brdf.fs");
 	background_shader   .Load("shaders\\background.vs", "shaders\\background.fs");
-
-	////diffuse_map   = new Texture;
-	////specular_map  = new Texture;
-
+	
 	albedo_map    = new Texture;
 	normal_map    = new Texture;
 	metallic_map  = new Texture;
@@ -210,29 +196,13 @@ void Prepare()
 	ao_map        = new Texture;
 
 	const std::string material("gold");
-
-	////diffuse_map   ->Load("textures\\materials\\" + material + "\\albedo.png");
-	////specular_map  ->Load("textures\\materials\\" + material + "\\metallic.png");
-
+	
 	albedo_map    ->Load("textures\\materials\\" + material + "\\albedo.png");
 	normal_map    ->Load("textures\\materials\\" + material + "\\normal.png");
 	metallic_map  ->Load("textures\\materials\\" + material + "\\metallic.png");
 	roughness_map ->Load("textures\\materials\\" + material + "\\roughness.png");
 	ao_map        ->Load("textures\\materials\\" + material + "\\ao.png");
-
-	////simple_shader.Use();
-	////simple_shader.SetVec3("color", glm::vec3(1.0, 0.807843, 0.407843));
-
-	////phong_shader.Use();
-	////phong_shader.SetVec3("light.position", glm::vec3(1.0f, 1.0f, 1.0f));
-	////phong_shader.SetVec3("light.ambient",  glm::vec3(1.0f, 1.0f, 1.0f));
-	////phong_shader.SetVec3("light.diffuse",  glm::vec3(1.0f, 1.0f, 1.0f));
-	////phong_shader.SetVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
-	////phong_shader.SetInt("material.diffuse",  0);
-	////phong_shader.SetInt("material.specular", 1);
-	////phong_shader.SetInt("material.normal",   2);
-	////phong_shader.SetFloat("material.shininess", 64.0f);
-
+	
 	pbr_shader.Use();
 	pbr_shader.SetVec3("light.position", glm::vec3(1.0f, 1.0f, 1.0f));
 	pbr_shader.SetVec3("light.diffuse",  glm::vec3(1.0f, 1.0f, 1.0f));
@@ -250,19 +220,6 @@ void Prepare()
 	background_shader.SetInt("environment_map", 0);
 
 	skybox = new Skybox;
-
-	////cubemap = new Cubemap(width, height);
-	////std::vector<std::string> cubemap_faces =
-	////{
-	////	"textures\\skybox\\right.jpg",
-	////	"textures\\skybox\\left.jpg",
-	////	"textures\\skybox\\top.jpg",
-	////	"textures\\skybox\\bottom.jpg",
-	////	"textures\\skybox\\front.jpg",
-	////	"textures\\skybox\\back.jpg",
-	////};
-	////cubemap->Load(cubemap_faces, false);
-
 	sphere = new Sphere;
 
 	glGenFramebuffers(1, &FBO);
@@ -394,9 +351,6 @@ void Prepare()
 
 void Cleanup()
 {
-	////delete diffuse_map;
-	////delete specular_map;
-
 	delete albedo_map;
 	delete normal_map;
 	delete metallic_map;
@@ -408,11 +362,13 @@ void Cleanup()
 	delete irradiance_map;
 	delete prefilter_map;
 	delete brdfLUT_texture;
-
-	//delete cubemap;
+	
 	delete skybox;
 	delete sphere;
 	delete quad;
+	
+	glDeleteFramebuffers(1, &FBO);
+	glDeleteRenderbuffers(1, &RBO);
 }
 
 //==============================================================================
@@ -427,22 +383,7 @@ void Render()
 	const auto model      = glm::scale(glm::mat4(1.0f), glm::vec3(0.5, 0.5, 0.5));
 	const auto view       = camera.GetView();
 	const auto projection = camera.GetProjection(aspect);
-
-	////simple_shader.Use();
-	////simple_shader.SetMat4("model", model);
-	////simple_shader.SetMat4("view", view);
-	////simple_shader.SetMat4("projection", projection);
-
-	////phong_shader.Use();
-	////phong_shader.SetMat4("model", model);
-	////phong_shader.SetMat4("view", view);
-	////phong_shader.SetMat4("projection", projection);
-	////phong_shader.SetVec3("camera", camera.GetPosition());
-
-	////diffuse_map  ->Bind(0);
-	////specular_map ->Bind(1);
-	////normal_map   ->Bind(2);
-
+	
 	pbr_shader.Use();
 	pbr_shader.SetMat4("model", model);
 	pbr_shader.SetMat4("view", view);
@@ -460,26 +401,13 @@ void Render()
 	ao_map          ->Bind(7);
 
 	sphere->Draw();
-
-	////cubemap->Bind(0);
-
-	////skybox_shader.Use();
-	////skybox_shader.SetInt("skybox", 0);
-	////skybox_shader.SetMat4("view", view);
-	////skybox_shader.SetMat4("projection", projection);
-
+	
 	background_shader.Use();
 	background_shader.SetMat4("view", view);
 	background_shader.SetMat4("projection", projection);
 
 	env_cubemap->Bind(0);
-	//irradiance_map->Bind(0);
-	//prefilter_map->Bind(0);
 	
-	//// render BRDF map to screen
-	//brdf_shader.Use();
-	//quad->Draw();
-
 	glDepthFunc(GL_LEQUAL);
 	skybox->Draw();
 	glDepthFunc(GL_LESS);
